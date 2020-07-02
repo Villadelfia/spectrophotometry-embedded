d912c3d0cdef2a5a516fb79f7afeddc4b76c1857
Randy Thiemann <randy.thiemann@student.pxl.be>
2020-07-02 03:06:46 +0200

    add pycharm project and fix setup script

--- host/setup-env.bat
+++ host/setup-env.bat
@@ -2,3 +2,4 @@ conda create -n spectro python=3.8
 conda activate spectro
 conda install pip
 pip install -r requirements.txt
+python %PROGRAMDATA%\Anaconda3\env\spectro\Scripts\pywin32_postinstall.py -install
